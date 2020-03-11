bf121af5716585d5501191e812a8d7466cee17d3
clee <lcup@foxmail.com>
2020-03-11 11:47:48 +0800

    购物车item展示

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,13 +1,47 @@
 <template>
-  <h2>购物车</h2>
+  <div class="cart">
+
+    <!--导航-->
+    <NavBar class="nav-bar">
+      <div slot="center">购物车({{length}})</div>
+    </NavBar>
+
+    <!--商品列表-->
+    <cart-list/>
+
+    <!--底部汇总-->
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Cart"
+  import NavBar from "components/common/nav/NavBar";
+  import CartList from "./childComps/CartList";
+
+  import {mapGetters} from 'vuex'
+
+  export default {
+    name: "Cart",
+    components:{
+      CartList,
+      NavBar
+    },
+    computed:{
+      // ...mapGetters(['cartLength','cartList'])
+      ...mapGetters({
+        length:'cartLength'
+      })
     }
+  }
 </script>
 
 <style scoped>
+  .cart{
+    height: 100vh;
 
+  }
+  .nav-bar{
+    background-color: var(--color-tint);
+    color: white;
+    /*font-weight: 700;*/
+  }
 </style>
