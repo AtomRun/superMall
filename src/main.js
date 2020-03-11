08ac76f9c81fc33c3b56eee8ade7b9e837c56296
clee <lcup@foxmail.com>
2020-03-11 20:47:55 +0800

    Vue插件的使用

--- src/main.js
+++ src/main.js
@@ -4,12 +4,23 @@ import router from "./router";
 import store from "./store";
 import toast from 'components/common/toast'
 
+import VueLazyload from "vue-lazyload";
+import FastClick from 'fastclick'
+
 Vue.config.productionTip = false
 
 Vue.prototype.$bus = new Vue()
 
 //安装toast插件
 Vue.use(toast)
+
+//解决移动端300ms延迟
+FastClick.attach(document.body)
+
+//使用懒加载的插件
+Vue.use(VueLazyload,{
+  loading:require('./assets/img/common/placeholder.png')
+})
 
 new Vue({
   render: h => h(App),
